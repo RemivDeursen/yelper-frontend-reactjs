61cde4b08a8085af366b2a8935ac8ed8a9b431ce
Remi van Deursen <cryslo1994@gmail.com>
2023-04-17 07:14:08 +0200

    Login and messaging page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import logo from './logo.svg';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+// noinspection ES6CheckImport
+import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
 import HomePage from "./views/HomePage";
+import Profile from "./views/Profile";
+import Pages from "./views/Pages";
+import Sidebar from "./components/Sidebar";
+import {Container, Row, Col, Card, Form, Button} from "react-bootstrap";
+import LoginModal from "./popups/LoginModal";
+import RegisterModal from "./popups/RegisterModal";
+
+// Styling
 import './styles/App.css';
 
 function App() {
-  return (
-    <Router>
-    <div>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        {/*<Route path="/login" element={<LoginModal />} />*/}
-        {/*<Route path="/register" element={<RegisterModal />} />*/}
-      </Routes>
-    </div>
-  </Router>
-  );
+    return (
+        <Router>
+            <div>
+                <Container fluid>
+                    <Row>
+                        <Col xs={2} id="sidebar-wrapper">
+                            <Sidebar/>
+                        </Col>
+                        <Col xs={10} id="page-content-wrapper">
+                            <Routes>
+                                <Route path="/" element={<Navigate to="/homepage" />} />
+                                <Route path="/homepage" element={<HomePage/>}/>
+                                <Route path="/profile" element={<Profile/>}/>
+                                <Route path="/pages" element={<Pages/>}/>
+                                <Route path="/login" element={<LoginModal />} />
+                                <Route path="/register" element={<RegisterModal />} />
+                            </Routes>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -3,9 +3,6 @@ import React, { useState, useEffect } from "react";
 import db from "../util/MockDB";
 import axios from "axios";
 import Thread from "../components/Thread";
-import Sidebar from "../components/Sidebar";
-import Header from "../components/Header";
-import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
 // Styling
 import '../styles/HomePage.css';
 
@@ -24,17 +21,7 @@ function HomePage() {
 
     return (
         <>
-            <Container fluid>
-                <Row>
-                    <Col xs={2} id="sidebar-wrapper">
-                        <Sidebar />
-                    </Col>
-                    <Col xs={10} id="page-content-wrapper">
-                        <Thread />
-                    </Col>
-                </Row>
-
-            </Container>
+            <Thread />
         </>
     );
 }
